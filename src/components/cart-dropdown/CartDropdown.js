7455f97a47f22e6b43bbfc261b26e66122a80ba3
Josh Sample <joshua.sample1@globalpay.com>
2020-02-24 20:19:48 -0500

    Created cart items and cart dropdown display

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import CustomButton from '../custom-button/CustomButton'
+import CartItem from '../cart-item/CartItem'
 
 import './CartDropdown.scss'
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
+  const renderCartItems = () => {
+    return cartItems.map(item => <CartItem key={item.id} item={item} />)
+  }
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">{renderCartItems()}</div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
-
-export default CartDropdown
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+})
+export default connect(mapStateToProps)(CartDropdown)
